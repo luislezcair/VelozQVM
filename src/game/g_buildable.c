ca3c5243a184125882c579da393739d14d5d695c
Luis Lezcano Airaldi <luislezcair@gmail.com>
2010-11-11 22:19:52 +0000

    Fixed a crash when using .so and killing some buildables

--- src/game/g_buildable.c
+++ src/game/g_buildable.c
@@ -647,7 +647,7 @@ void ASpawn_Die( gentity_t *self, gentity_t *inflictor, gentity_t *attacker, int
   VectorCopy( self->s.angles, new->angles );
   VectorCopy( self->s.origin2, new->origin2 );
   VectorCopy( self->s.angles2, new->angles2 );
-  new->fate = ( attacker->client->ps.stats[ STAT_PTEAM ] == PTE_ALIENS ) ? BF_TEAMKILLED : BF_DESTROYED;
+  new->fate = ( attacker && attacker->client && attacker->client->ps.stats[ STAT_PTEAM ] == PTE_ALIENS ) ? BF_TEAMKILLED : BF_DESTROYED;
   new->next = NULL;
   G_LogBuild( new );
 
@@ -948,7 +948,7 @@ void ABarricade_Die( gentity_t *self, gentity_t *inflictor, gentity_t *attacker,
   VectorCopy( self->s.angles, new->angles );
   VectorCopy( self->s.origin2, new->origin2 );
   VectorCopy( self->s.angles2, new->angles2 );
-  new->fate = ( attacker->client->ps.stats[ STAT_PTEAM ] == PTE_ALIENS ) ? BF_TEAMKILLED : BF_DESTROYED;
+  new->fate = ( attacker && attacker->client && attacker->client->ps.stats[ STAT_PTEAM ] == PTE_ALIENS ) ? BF_TEAMKILLED : BF_DESTROYED;
   new->next = NULL;
   G_LogBuild( new );
    
@@ -1381,7 +1381,7 @@ void AHovel_Die( gentity_t *self, gentity_t *inflictor, gentity_t *attacker, int
   VectorCopy( self->s.angles, new->angles );
   VectorCopy( self->s.origin2, new->origin2 );
   VectorCopy( self->s.angles2, new->angles2 );
-  new->fate = ( attacker->client->ps.stats[ STAT_PTEAM ] == PTE_ALIENS ) ? BF_TEAMKILLED : BF_DESTROYED;
+  new->fate = ( attacker && attacker->client && attacker->client->ps.stats[ STAT_PTEAM ] == PTE_ALIENS ) ? BF_TEAMKILLED : BF_DESTROYED;
   new->next = NULL;
   G_LogBuild( new );
 
@@ -2510,7 +2510,7 @@ void HSpawn_Die( gentity_t *self, gentity_t *inflictor, gentity_t *attacker, int
   VectorCopy( self->s.angles, new->angles );
   VectorCopy( self->s.origin2, new->origin2 );
   VectorCopy( self->s.angles2, new->angles2 );
-  new->fate = ( attacker->client->ps.stats[ STAT_PTEAM ] == PTE_HUMANS ) ? BF_TEAMKILLED : BF_DESTROYED;
+  new->fate = ( attacker && attacker->client && attacker->client->ps.stats[ STAT_PTEAM ] == PTE_HUMANS ) ? BF_TEAMKILLED : BF_DESTROYED;
   new->next = NULL;
   G_LogBuild( new );
   

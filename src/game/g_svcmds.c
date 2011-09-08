c2cf2f71b2981c0b9c935468132774f4e05fd5ce
luislezcair <luislezcair@3a716346-af6c-c063-c4ff-8573fef58820>
2011-09-08 00:34:27 +0000

    Fixed a bug in the status command

--- src/game/g_svcmds.c
+++ src/game/g_svcmds.c
@@ -312,7 +312,7 @@ static void Svcmd_Status_f( void )
 
     trap_GetUserinfo( i, userinfo, sizeof( userinfo ) );
     G_Printf( "%-21s ", Info_ValueForKey( userinfo, "ip" ) );
-    G_Printf( "%-8d ", Info_ValueForKey( userinfo, "rate" ) );
+    G_Printf( "%-8s ", Info_ValueForKey( userinfo, "rate" ) );
     G_Printf( "%s\n", cl->pers.netname );
   }
 }
